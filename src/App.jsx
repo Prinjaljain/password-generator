2ddb663e20e81d79874c00f4d5e3626d2235cbf8
Prinjaljain <prinjaljain2107@gmail.com>
2024-03-09 18:51:56 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-3xl bg-orange-500'>Currency Converter</h1>
+    </div>
+  )
+}
+
+export default App
